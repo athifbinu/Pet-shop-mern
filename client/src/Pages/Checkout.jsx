66578e22c78672da9ed9060fb01218a70e7385aa
Athif Binu <binuathif@gmail.com>
2023-10-27 12:26:14 +0530

    npm add

--- client/src/Pages/Checkout.jsx
+++ client/src/Pages/Checkout.jsx
@@ -55,27 +55,16 @@ const Checkout = () => {
               Checkout
             </p>
             <p class="text-base leading-normal sm:leading-4 text-gray-600 dark:text-white">
-              Home{" "}
+
             </p>
           </div>
 
           <div class="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
-            <div class="xl:w-3/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
-              <div class="flex flex-col justify-start items-start w-full space-y-4">
-                <p class="text-xl md:text-2xl leading-normal text-gray-800 dark:text-gray-50">
-                  Logitech K251
-                </p>
-                <p class="text-base font-semibold leading-none text-gray-600 dark:text-white">
-                  $520.00
-                </p>
-              </div>
-              <div class="mt-6 sm:mt-0 xl:my-10 xl:px-20 w-52 sm:w-96 xl:w-auto">
-                <img
-                  src="https://i.ibb.co/0GFzTP4/Rectangle-131.png"
-                  alt="headphones"
-                />
-              </div>
-            </div>
+           
+
+           
+
+
 
             <div class="p-8 bg-gray-100 dark:bg-gray-800 flex flex-col lg:w-full xl:w-3/5">
               <button class="border border-transparent hover:border-gray-300 bg-gray-900 dark:bg-white dark:hover:bg-gray-900 dark:hover:border-gray-900 dark:text-gray-900 dark:hover:text-white hover:bg-white text-white hover:text-gray-900 flex flex-row justify-center items-center space-x-2 py-4 rounded w-full">
